82d0f3ad9b30769a7a86849cc29aa8e65b7ff556
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-03-07 19:38:23 +0300

    handled spaces before dots in the other function

--- bert_pretraining/sentence_tokenizer2.py
+++ bert_pretraining/sentence_tokenizer2.py
@@ -1,9 +1,11 @@
 import nltk
 import sys
+import re
 
 st2 = nltk.data.load('tokenizers/punkt/english.pickle')
 
 for line in sys.stdin:
+    line = re.sub(r'\s([?.!"](?:\s|$))', r'\1', line)
     sent_tokenize_list = st2.tokenize(line, realign_boundaries=True)
     for sentence in sent_tokenize_list:
         print(sentence)
