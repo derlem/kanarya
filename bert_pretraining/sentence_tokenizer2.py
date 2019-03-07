b6a9b8345b0b6b35481bff4fd63ff80822475b07
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-03-07 20:24:48 +0300

    updates sentence_tokenizer2.py

--- bert_pretraining/sentence_tokenizer2.py
+++ bert_pretraining/sentence_tokenizer2.py
@@ -6,7 +6,11 @@ st2 = nltk.data.load('tokenizers/punkt/english.pickle')
 
 for line in sys.stdin:
     line = re.sub(r'\s([?.!"](?:\s|$))', r'\1', line)
-    sent_tokenize_list = st2.tokenize(line, realign_boundaries=True)
+    line = re.sub('\' \'', '', line)
+    line = re.sub('` `', '', line)
+    line = re.sub('``', '', line)    sent_tokenize_list = st2.tokenize(line, realign_boundaries=True)
     for sentence in sent_tokenize_list:
-        print(sentence)
-        print()
+        if sentence[:-1].isdigit():
+            print(sentence, end=" ")
+        else:
+            print(sentence)
