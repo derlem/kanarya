5e460d73fc134e19788f4fd1938a3ce6ee2d6d3d
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-03-11 22:01:42 +0300

    added --input_sentence_size to the sentencepiece_trainer.py

--- bert_pretraining/sentencepiece_trainer.py
+++ bert_pretraining/sentencepiece_trainer.py
@@ -2,17 +2,26 @@
 import sentencepiece as spm
 import argparse
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 if __name__ == "__main__":
 
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--input")
     parser.add_argument("--model_prefix", default="m")
     parser.add_argument("--vocab_size", type=int, default=28996)
     parser.add_argument("--model_type", default="bpe")
-    #parser.add_argument("--input_sentence_size", type=int)
+    parser.add_argument("--input_sentence_size", type=int)
     parser.add_argument("--control_symbols", default="[SEP],[CLS],[PAD],[MASK]")
 
     args = parser.parse_args()
+
+    sentence_size = file_len(args.input)
 
     spm.SentencePieceTrainer.Train \
         ("--input=%s "
@@ -20,6 +29,6 @@ if __name__ == "__main__":
          "--vocab_size=%d "
          "--character_coverage=1.0 "
          "--model_type=%s "
-         #"--input_sentence_size=%d "
+         "--input_sentence_size=%d "
          "--control_symbols=%s" %
-        (args.input, args.model_prefix, int(args.vocab_size), args.model_type, args.control_symbols))
+        (args.input, args.model_prefix, int(args.vocab_size), args.model_type, sentence_size, args.control_symbols))
