88b20becaca385d8e476320bdf78e749a449b49d
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-03-20 16:08:25 +0300

    added current file information while creating pretraining data on file chunks

--- bert_pretraining/run_create_pretraining_data_chunks.sh
+++ bert_pretraining/run_create_pretraining_data_chunks.sh
@@ -8,7 +8,9 @@ for i in {1..12}
 do
 	input_file="$input_file$i.txt"
 	output_file="$output_file$i.tfrecord"
+	echo "Started creating pretraining data for file: $input_file"
 	./run_create_pretraining_data.sh $input_file $output_file $vocab_file
+	echo "Completed creating pretraining data for file: $input_file"
 	input_file="../sentence_split_chunk"
 	output_file="pretraining_datas/data"
 done
