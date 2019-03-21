0a8047cc55ffc6346513d64c5efe04fa6ebd889a
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-03-21 12:55:59 +0300

    some changes on the create pretraining data on chunks script

--- bert_pretraining/run_create_pretraining_data_chunks.sh
+++ bert_pretraining/run_create_pretraining_data_chunks.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-input_file="../sentence_split_chunk"
+input_file="../chunks/chunk"
 vocab_file="../m.model"
 output_file="pretraining_datas/data"
 
 ../sentence_split_chunk1.txt pretraining_datas/data1.tfrecord ../m.model 
-for i in {1..12}
+for i in {1..51}
 do
-	input_file="$input_file$i.txt"
+	input_file="$input_file_$i.txt"
 	output_file="$output_file$i.tfrecord"
 	echo "Started creating pretraining data for file: $input_file"
 	./run_create_pretraining_data.sh $input_file $output_file $vocab_file
 	echo "Completed creating pretraining data for file: $input_file"
-	input_file="../sentence_split_chunk"
+	input_file="../chunks/chunk"
 	output_file="pretraining_datas/data"
 done
