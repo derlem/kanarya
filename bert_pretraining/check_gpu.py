b9be0ad1ecccfe53e45499b5deabd8cf3e787341
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-03-23 16:53:30 +0300

    added check gpu script

--- bert_pretraining/check_gpu.py
+++ bert_pretraining/check_gpu.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
      
-print("VERSION", tf.__version__sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)))
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
