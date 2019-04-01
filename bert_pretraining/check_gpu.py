6cab1295fb9149a944ae967a8f4b7ac6c3f262a7
ugurcanarikan <ugurcanarikan95@gmail.com>
2019-04-01 17:12:35 +0000

    changed directory in the create pretraining data script, deleted unnecessary files

--- bert_pretraining/check_gpu.py
+++
@@ -1,3 +0,0 @@
-import tensorflow as tf
-     
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
--- bert_pretraining/rename_files.py
+++
@@ -1,8 +0,0 @@
-import os
-
-files = os.listdir('.')
-files.sort()
-counter = 1
-for file in files:
-	os.rename(os.path.basename(file), "chunk_" + str(counter) + ".txt")
-	counter = counter + 1
