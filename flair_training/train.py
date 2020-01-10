9e49825fd6648378c97fdb9caf5a10b6b8bec3b1
onur güngör <onurgu@gmail.com>
2020-01-11 00:03:01 +0300

    many contributions
    - dropout
    - zor_cumleler.txt evaluation
    - evaluate_from_stdin
    - hidden size
    - annealing parameters tuned
    - adamw

--- flair_training/train.py
+++ flair_training/train.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 from pathlib import Path
@@ -5,7 +6,7 @@ from typing import List
 
 
 import flair
-from flair.data import TaggedCorpus, MultiCorpus, Dictionary
+from flair.data import TaggedCorpus, MultiCorpus, Dictionary, Sentence
 from flair.data_fetcher import NLPTaskDataFetcher
 from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, BertEmbeddings, CharacterEmbeddings
 from flair.hyperparameter import SearchSpace, Parameter, SequenceTaggerParamSelector
@@ -16,12 +17,14 @@ from flair.training_utils import EvaluationMetric
 from hyperopt import hp
 import numpy as np
 import torch
-from torch.optim import Adam, SGD
+from torch.optim import Adam, SGD, AdamW
+
+log = logging.getLogger('flair')
 
 data_folder = '../data'
 tag_type = 'ner'
 
-optimizers_dict = {"sgd": SGD, "adam": Adam}
+optimizers_dict = {"sgd": SGD, "adam": Adam, "adamw": AdamW}
 
 
 def load_standard_corpus(data_folder, tag_type):
@@ -32,19 +35,37 @@ def load_standard_corpus(data_folder, tag_type):
     for key, value in files.items():
         files[key] = os.path.abspath(os.path.join(data_folder, value))
 
-    corpus = load_specific_corpus(files)
+    corpus = load_specific_corpus(data_folder, files)
 
     tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
 
-    print(corpus.obtain_statistics(tag_type=tag_type))
+    log.info(corpus.obtain_statistics(tag_type=tag_type))
 
     return corpus, tag_dictionary
 
 
-def load_specific_corpus(files):
+def load_specific_corpus(dirname, files):
 
     columns = {0: 'text', 1: 'ner'}
-    corpus: TaggedCorpus = NLPTaskDataFetcher.load_column_corpus("/", columns, **files)
+    if files['train_file']:
+        train_file = os.path.basename(files['train_file'])
+    else:
+        train_file = None
+    if files['dev_file']:
+        dev_file = os.path.basename(files['dev_file'])
+    else:
+        dev_file = None
+    if files['test_file']:
+        test_file = os.path.basename(files['test_file'])
+    else:
+        test_file = None
+
+    corpus: TaggedCorpus = NLPTaskDataFetcher.load_column_corpus(dirname,
+                                                                 columns,
+                                                                 train_file=train_file,
+                                                                 dev_file=dev_file,
+                                                                 test_file=test_file)
+    log.info(corpus)
 
     return corpus
 
@@ -76,7 +97,8 @@ def create_embeddings(params):
 
 def create_model(params, tag_dictionary):
     embeddings = create_embeddings(params)
-    tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+    tagger: SequenceTagger = SequenceTagger(hidden_size=params["hidden_size"],
+                                            dropout=params["dropout"],
                                             embeddings=embeddings,
                                             tag_dictionary=tag_dictionary,
                                             tag_type=params["tag_type"],
@@ -88,7 +110,7 @@ def create_model(params, tag_dictionary):
 
 def load_model(model_output_dirpath):
     final_model_filepath = os.path.join(model_output_dirpath, "final-model.pt")
-    tagger = SequenceTagger.load_from_file(final_model_filepath)
+    tagger: SequenceTagger = SequenceTagger.load_from_file(final_model_filepath)
     return tagger
 
 
@@ -161,6 +183,10 @@ def train(params, tagger, corpus):
                   learning_rate=params["learning_rate"],
                   mini_batch_size=params["mini_batch_size"],
                   max_epochs=params["max_epochs"],
+                  patience=10,
+                  anneal_factor=0.5,
+                  anneal_against_train_loss=True,
+                  anneal_with_restarts=True,
                   checkpoint=True)
 
 
@@ -186,13 +212,17 @@ def main():
                                               "find_learning_rate",
                                               "train",
                                               "resume_train",
-                                              "evaluate"], required=True)
+                                              "evaluate",
+                                              "evaluate_from_stdin",
+                                              "evaluate_zor_cumleler"], required=True)
     parser.add_argument("--embedding_type", choices=["bert", "flair", "char"])
     parser.add_argument("--model_name", default="default_model_name")
     parser.add_argument("--bert_model_dirpath_or_name", default="bert-base-multilingual-cased")
     parser.add_argument("--model_output_dirpath", default=None)
     parser.add_argument("--other_test_file_for_evaluation", default=None)
-    parser.add_argument("--optimizer", default="sgd", choices=["sgd", "adam"])
+    parser.add_argument("--optimizer", default="sgd", choices=["sgd", "adam", "adamw"])
+    parser.add_argument("--hidden_size", default=256, type=int)
+    parser.add_argument("--dropout", default=0.5, type=float)
     parser.add_argument("--learning_rate", default=0.05, type=float)
     parser.add_argument("--max_epochs", default=10, type=int)
     parser.add_argument("--mini_batch_size", default=16, type=int)
@@ -200,6 +230,7 @@ def main():
     parser.add_argument("--data_folder", default="./data")
 
     parser.add_argument("--device", default="gpu", choices=["cpu", "gpu"])
+    parser.add_argument("--loglevel", default="NOTSET", choices=["critical", "NOTSET"])
 
     args = parser.parse_args()
 
@@ -219,11 +250,18 @@ def main():
     other_test_file_for_evaluation = args.other_test_file_for_evaluation
 
     optimizer = args.optimizer
+    hidden_size = args.hidden_size
+    dropout = args.dropout
     learning_rate = args.learning_rate
     max_epochs = args.max_epochs
     mini_batch_size = args.mini_batch_size
     data_folder = args.data_folder
     device = args.device
+    loglevel = args.loglevel
+
+    log.setLevel(loglevel)
+    if command in ["evaluate", "evaluate_from_stdin", "evaluate_zor_cumleler"]:
+        log.setLevel("CRITICAL")
 
     if device != "gpu" and device == "cpu":
         flair.device = torch.device('cpu')
@@ -231,11 +269,14 @@ def main():
     if other_test_file_for_evaluation is None:
         corpus, tag_dictionary = load_standard_corpus(data_folder, tag_type)
     else:
-        corpus = load_specific_corpus({"test_file": other_test_file_for_evaluation})
+        dirname = os.path.dirname(other_test_file_for_evaluation)
+        corpus = load_specific_corpus(dirname, {"train_file": None,
+                                                "dev_file": None,
+                                                "test_file": os.path.basename(other_test_file_for_evaluation)})
         # tag dictionary will be loaded from the model
         tag_dictionary = None
 
-    assert corpus and command != "evaluate", "corpus should be loaded"
+    assert corpus, "corpus should be loaded"
 
     params = {
         "model_name": model_name,
@@ -244,6 +285,8 @@ def main():
         "bert_model_dirpath_or_name": bert_model_dirpath_or_name,
         "model_output_dirpath": model_output_dirpath,
         "optimizer": optimizer,
+        "hidden_size": hidden_size,
+        "dropout": dropout,
         "learning_rate": learning_rate,
         "max_epochs": max_epochs,
         "mini_batch_size": mini_batch_size
@@ -287,15 +330,69 @@ def main():
             tag_dictionary: Dictionary = Dictionary.load_from_file(os.path.join(model_output_dirpath,
                                                                             "tag_dictionary.pickle"))
         except FileNotFoundError:
-            print("WARN: tag_dictionary is not found at %s" % os.path.join(model_output_dirpath,
+            log.warning("WARN: tag_dictionary is not found at %s" % os.path.join(model_output_dirpath,
                                                                             "tag_dictionary.pickle"))
         params = load_params(os.path.join(model_output_dirpath,
                                           "params.json"))
         tagger: SequenceTagger = load_model(model_output_dirpath)
 
         trainer = create_trainer(tagger, corpus)
-        trainer.evaluate(tagger, corpus.test, eval_mini_batch_size=16,
-                         out_path=os.path.join(params["model_output_dirpath"], "evaluation.txt"))
+        if other_test_file_for_evaluation:
+            out_path = other_test_file_for_evaluation + ".evaluation"
+        else:
+            out_path = os.path.join(params["model_output_dirpath"], "evaluation.txt")
+        metric, eval_loss = trainer.evaluate(tagger, corpus.test, eval_mini_batch_size=16,
+                         out_path=Path(out_path))
+
+        print(metric, eval_loss)
+    elif command == "evaluate_from_stdin":
+        try:
+            tag_dictionary: Dictionary = Dictionary.load_from_file(os.path.join(model_output_dirpath,
+                                                                                "tag_dictionary.pickle"))
+        except FileNotFoundError:
+            print("WARN: tag_dictionary is not found at %s" % os.path.join(model_output_dirpath,
+                                                                           "tag_dictionary.pickle"))
+        params = load_params(os.path.join(model_output_dirpath,
+                                          "params.json"))
+        tagger: SequenceTagger = load_model(model_output_dirpath)
+
+        import sys
+        line = sys.stdin.readline().strip()
+        while line:
+            sentence = Sentence(line, use_tokenizer=True)
+            tagger.predict([sentence], mini_batch_size=1, verbose=True)
+            # print(sentence)
+            # print(sentence.to_tagged_string())
+            for token in sentence.tokens:
+                tag = token.get_tag(tag_type)
+                print(token.text, 'O', tag.value, tag.score)
+            print()
+            line = sys.stdin.readline().strip()
+
+    elif command == "evaluate_zor_cumleler":
+        try:
+            tag_dictionary: Dictionary = Dictionary.load_from_file(os.path.join(model_output_dirpath,
+                                                                                "tag_dictionary.pickle"))
+        except FileNotFoundError:
+            log.warning("WARN: tag_dictionary is not found at %s" % os.path.join(model_output_dirpath,
+                                                                           "tag_dictionary.pickle"))
+        params = load_params(os.path.join(model_output_dirpath,
+                                          "params.json"))
+        tagger: SequenceTagger = load_model(model_output_dirpath)
+
+        metrics = {"TP": 0, "FP": 0, "TN": 0, "FN": 0}
+
+        with open("data/zor_cumleler.txt") as f:
+            line = f.readline().strip()
+            while line:
+                sentence = Sentence(line, use_tokenizer=True)
+                tagger.predict([sentence], mini_batch_size=1, verbose=False)
+                if len(sentence.get_spans(tag_type)) > 0:
+                    metrics["TP"] += 1
+                else:
+                    metrics["FN"] += 1
+                line = f.readline().strip()
+        print(metrics)
 
 
 if __name__ == "__main__":
