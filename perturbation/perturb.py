e78489c0435c31db4715a6be14dc63689be43da3
Hasan Ozturk <hasan.ozturk@boun.edu.tr>
2020-03-29 22:20:46 +0000

    Implement perturbation strategies using bert vocab file

--- perturbation/perturb.py
+++ perturbation/perturb.py
@@ -68,10 +68,10 @@ def populate_vocab(params):
 		line = line.strip() # Eliminate space chars
 		if line[:2] == "##":
 			bert_vocab_subtokens.add(line[2:])
-			#print("Subtoken: " + line[2:] + " - " + str(len(line[2:])))
+			##print("Subtoken: " + line[2:] + " - " + str(len(line[2:])))
 		else:
 			bert_vocab_tokens.add(line)
-			#print("Token: " + line + " - " + str(len(line)))
+			##print("Token: " + line + " - " + str(len(line)))
 
 
 
@@ -84,7 +84,7 @@ def div_dict(my_dict, dividend):
 def get_prob(sentence, pos):
 
 	deda_token = sentence.tokens[pos]
-	#print("Token: " + str(deda_token))
+	##print("Token: " + str(deda_token))
 	label = deda_token.get_tag('ner') # Be careful, it returns 
 
 	value = label.value
@@ -94,7 +94,7 @@ def get_prob(sentence, pos):
 	else:
 		score = 1 - label.score
 
-	#print(str(deda_token) + ": " + str(label))
+	##print(str(deda_token) + ": " + str(label))
 	return score # float
 
 
@@ -107,7 +107,7 @@ def perturb1(sentence, pos):
 	token = perturbed_sentence.tokens[perturbed_token_pos]
 	if len(token.text) != 1:
 		token.text = token.text[:-1]
-	#print("Perturb 1: " + str(perturbed_sentence))
+	##print("Perturb 1: " + str(perturbed_sentence))
 	return perturbed_sentence
 
 
@@ -120,7 +120,7 @@ def perturb2(sentence, pos):
 	token = perturbed_sentence.tokens[perturbed_token_pos]
 	if len(token.text) != 1:
 		token.text = token.text[1:]
-	#print("Perturb 2: " + str(perturbed_sentence))
+	##print("Perturb 2: " + str(perturbed_sentence))
 	return perturbed_sentence
 
 # Capitalize the first character of the preceding word
@@ -131,7 +131,7 @@ def perturb3(sentence, pos):
 	perturbed_sentence = copy.deepcopy(sentence)
 	token = perturbed_sentence.tokens[perturbed_token_pos]
 	token.text = token.text.capitalize()
-	#print("Perturb 3: " + str(perturbed_sentence))
+	##print("Perturb 3: " + str(perturbed_sentence))
 	return perturbed_sentence
 
 # !!! If there is no word in the position where a word will be deleted, then we return delta_pi = 0, which might cause problems.
@@ -143,10 +143,10 @@ def perturb4(sentence, pos):
 	if pos > 0:
 		perturbed_sentence = copy.deepcopy(sentence)
 		del perturbed_sentence.tokens[pos - 1]
-		#print("Perturb 4: " + str(perturbed_sentence))
+		##print("Perturb 4: " + str(perturbed_sentence))
 		return perturbed_sentence
 	else:
-		#print("Perturb 4: " + str(sentence))
+		##print("Perturb 4: " + str(sentence))
 		return sentence
 
 # Remove the two previous word, if any
@@ -156,10 +156,10 @@ def perturb5(sentence, pos):
 	if pos > 1:
 		perturbed_sentence = copy.deepcopy(sentence)
 		del perturbed_sentence.tokens[pos - 2]
-		#print("Perturb 5: " + str(perturbed_sentence))
+		##print("Perturb 5: " + str(perturbed_sentence))
 		return perturbed_sentence
 	else:
-		#print("Perturb 5: " + str(sentence))
+		##print("Perturb 5: " + str(sentence))
 		return sentence
 
 
@@ -171,10 +171,10 @@ def perturb6(sentence, pos):
 	if pos > 2:
 		perturbed_sentence = copy.deepcopy(sentence)
 		del perturbed_sentence.tokens[pos - 3]
-		#print("Perturb 6: " + str(perturbed_sentence))
+		##print("Perturb 6: " + str(perturbed_sentence))
 		return perturbed_sentence
 	else:
-		#print("Perturb 6: " + str(sentence))
+		##print("Perturb 6: " + str(sentence))
 		return sentence
 
 # Remove the following word, if any
@@ -184,10 +184,10 @@ def perturb7(sentence, pos):
 	if pos < (len(sentence.tokens) - 1):
 		perturbed_sentence = copy.deepcopy(sentence)
 		del perturbed_sentence.tokens[pos + 1]
-		#print("Perturb 7: " + str(perturbed_sentence))
+		##print("Perturb 7: " + str(perturbed_sentence))
 		return perturbed_sentence
 	else:
-		#print("Perturb 7: " + str(sentence))
+		##print("Perturb 7: " + str(sentence))
 		return sentence
 
 # Remove the two following word, if any
@@ -197,10 +197,10 @@ def perturb8(sentence, pos):
 	if pos < (len(sentence.tokens) - 2):
 		perturbed_sentence = copy.deepcopy(sentence)
 		del perturbed_sentence.tokens[pos + 2]
-		#print("Perturb 8: " + str(perturbed_sentence))
+		##print("Perturb 8: " + str(perturbed_sentence))
 		return perturbed_sentence
 	else:
-		#print("Perturb 8: " + str(sentence))
+		##print("Perturb 8: " + str(sentence))
 		return sentence
 
 # Remove the three following word, if any
@@ -210,10 +210,10 @@ def perturb9(sentence, pos):
 	if pos < (len(sentence.tokens) - 3):
 		perturbed_sentence = copy.deepcopy(sentence)
 		del perturbed_sentence.tokens[pos + 3]
-		#print("Perturb 9: " + str(perturbed_sentence))
+		##print("Perturb 9: " + str(perturbed_sentence))
 		return perturbed_sentence
 	else:
-		#print("Perturb 9: " + str(sentence))
+		##print("Perturb 9: " + str(sentence))
 		return sentence
 
 # Remove the first and shortest common pattern (##subtoken) in the preceding word using BERT vocab
@@ -229,13 +229,12 @@ def perturb10(sentence, pos):
 		token.text = perturbed_token
 		
 		
-		print("Perturb 10: " + str(perturbed_sentence))
+		#print("Perturb 10: " + str(perturbed_sentence)+ "\n")
 		return perturbed_sentence
 	else:
-		print("Perturb 10: " + str(sentence))
+		#print("Perturb 10: " + str(sentence)+ "\n")
 		return sentence
 
-	
 
 def perturb10_helper(word):
 
@@ -247,15 +246,267 @@ def perturb10_helper(word):
 		if idx > 1:
 			if word[:idx+1] in bert_vocab_subtokens:
 				if len(word[idx+1:]) > 0:
-					print("Word: " + word + " - " + "Found: " + word[:idx+1] + " - " + "Return: " + word[idx+1:])
+					#print("Perturb 10: Word: " + word + " - " + "Found: " + word[:idx+1] + " - " + "Return: " + word[idx+1:])
 					return word[idx+1:]
-	print("Could not find a pattern, returning the original word: " + word)
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+
+# Keep the first and shortest common pattern (##subtoken) in the preceding word using BERT vocab and remove the rest
+def perturb11(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos > 0:
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos - 1]
+		perturbed_token = perturb11_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 11: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 11: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb11_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			if word[:idx+1] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 11: Word: " + word + " - " + "Found: " + word[:idx+1] + " - " + "Return: " + word[:idx+1])
+					return word[:idx+1]
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+
+# Remove the last and shortest common pattern (##subtoken) in the preceding word using BERT vocab
+def perturb12(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos > 0:
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos - 1]
+		perturbed_token = perturb12_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 12: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 12: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb12_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			##print(word[-idx:])
+			if word[-(idx+1):] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 12: Word: " + word + " - " + "Found: " + word[-(idx+1):] + " - " + "Return: " + word[:-(idx+1)] )
+					return word[:-(idx+1):]
+
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+# Keep the last and shortest common pattern (##subtoken) in the preceding word using BERT vocab
+def perturb13(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos > 0:
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos - 1]
+		perturbed_token = perturb13_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 13: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 13: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb13_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			##print(word[-idx:])
+			if word[-(idx+1):] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 13: Word: " + word + " - " + "Found: " + word[-(idx+1):] + " - " + "Return: " + word[-(idx+1):] )
+					return word[-(idx+1):]
+					
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+
+# Remove the first and shortest common pattern (##subtoken) in the following word using BERT vocab
+def perturb14(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos < (len(sentence.tokens) - 1):
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos + 1]
+		perturbed_token = perturb14_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 14: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 14: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb14_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			if word[:idx+1] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 14: Word: " + word + " - " + "Found: " + word[:idx+1] + " - " + "Return: " + word[idx+1:] )
+					return word[idx+1:]
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+# Keep the first and shortest common pattern (##subtoken) in the following word using BERT vocab
+def perturb15(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos < (len(sentence.tokens) - 1):
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos + 1]
+		perturbed_token = perturb15_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 15: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 15: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb15_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			if word[:idx+1] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 15: Word: " + word + " - " + "Found: " + word[:idx+1] + " - " + "Return: " + word[:idx+1])
+					return word[:idx+1]
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+# Remove the last and shortest common pattern (##subtoken) in the following word using BERT vocab
+def perturb16(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos < (len(sentence.tokens) - 1):
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos + 1]
+		perturbed_token = perturb16_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 16: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 16: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb16_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			if word[-(idx+1):] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 16: Word: " + word + " - " + "Found: " + word[-(idx+1):] + " - " + "Return: " + word[:-(idx+1)] )
+					return word[:-(idx+1):]
+	#print("Could not find a pattern, returning the original word: " + word)
+	return word
+
+# Keep the last and shortest common pattern (##subtoken) in the following word using BERT vocab
+def perturb17(sentence, pos):
+
+	# If there is a preceding word, then remove it. If not, do not perturb
+	if pos < (len(sentence.tokens) - 1):
+		perturbed_sentence = copy.deepcopy(sentence)
+		
+		token = perturbed_sentence.tokens[pos + 1]
+		perturbed_token = perturb17_helper(token.text)
+
+		token.text = perturbed_token
+		
+		
+		#print("Perturb 17: " + str(perturbed_sentence)+ "\n")
+		return perturbed_sentence
+	else:
+		#print("Perturb 17: " + str(sentence)+ "\n")
+		return sentence
+
+
+def perturb17_helper(word):
+
+	# Careful w/ single chars
+
+	for idx in range(len(word)):
+
+		# For now ignore single chars, since it seems like there is a problem in bert vocab
+		if idx > 1:
+			if word[-(idx+1):] in bert_vocab_subtokens:
+				if len(word[idx+1:]) > 0:
+					#print("Perturb 17: Word: " + word + " - " + "Found: " + word[-(idx+1):] + " - " + "Return: " + word[-(idx+1):] )
+					return word[-(idx+1):]
+	#print("Could not find a pattern, returning the original word: " + word)
 	return word
 
 
 
 
-perturbation_functions = [perturb1, perturb2, perturb4, perturb5, perturb6, perturb7, perturb8, perturb9, perturb10]
+perturbation_functions = [perturb1, perturb2, perturb4, perturb5, perturb6, perturb7, perturb8, perturb9, perturb10, perturb11, perturb12, perturb13, perturb14, perturb15, perturb16, perturb17]
 
 
 def create_dict():
@@ -326,7 +577,7 @@ def runner(params):
 				deda_pos = get_deda_pos(sentence)
 				p0 = get_prob(sentence, deda_pos) 
 
-				#print(sentence)
+				##print(sentence)
 				for func in perturbation_functions:
 					perturbed_sentence = func(sentence, deda_pos)
 
@@ -337,11 +588,11 @@ def runner(params):
 
 					delta_p = pi - p0
 
-					#print("Delta_p for " + str(func) + ": " + str(delta_p))
+					##print("Delta_p for " + str(func) + ": " + str(delta_p))
 					#delta_p_dict[str(func)] += delta_p
 					delta_p_dict[str(func)].append(delta_p)
 
-				#print("=================================")
+				##print("=================================")
 				
 
 				sentence = ""
@@ -360,15 +611,16 @@ def report():
 
     print("Sentence Count: " + str(sentence_count) + "\n")
 
-    print('{:<30s}{:<30s}{:<30s}'.format("Function","Mean", "Variance"))
+    print('{:<50s}{:<30s}{:<30s}{:<30s}'.format("Function","Absolute Mean", "Direction", "Variance"))
     for func in perturbation_functions:
     	# Eliminate zeros (?)
     	delta_p_dict[str(func)] = [i for i in delta_p_dict[str(func)] if i != 0.0]
 
     	mean = np.mean(delta_p_dict[str(func)])
     	var = np.var(delta_p_dict[str(func)])
+    	direction = "+" if mean > 0 else "-"
 
-    	s = '{:<50s}{:<30s}{:<30s}'.format(str(func),str(mean), str(var))
+    	s = '{:<50s}{:<30s}{:<30s}{:<30s}'.format(str(func),str(abs(mean)), direction, str(var))
     	print(s)
 
 
@@ -407,11 +659,11 @@ def main():
     '''
     global delta_p_dict
 
-    print("Sentence Count: " + str(sentence_count))
+    #print("Sentence Count: " + str(sentence_count))
 
     div_dict(delta_p_dict, sentence_count)
-    # Pretty print
-    print (json.dumps(delta_p_dict, indent=4))
+    # Pretty #print
+    #print (json.dumps(delta_p_dict, indent=4))
     '''
 
 
