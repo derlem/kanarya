0c15588843a44882b0783b5b8c60b432720fa7e8
tony <hasan.ozturk@boun.edu.tr>
2020-05-17 21:16:34 +0300

    Make production settings

--- game_project/game_project/settings.py
+++ game_project/game_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'jr1tc)e$bblr__au!18_*#p%g%ysoe84y@1yq%cu$1vi4w%mnh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.198.136.255']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
