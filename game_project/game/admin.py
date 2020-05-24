5a8a8a46266a3ae8565662a645e4a3b0121bd9db
tony <hasan.ozturk@boun.edu.tr>
2020-05-24 20:17:44 +0300

    Open current models to admin access

--- game_project/game/admin.py
+++ game_project/game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sentence, Question, Activity, Decision, Report
+
+admin.site.register(Sentence)
+admin.site.register(Question)
+admin.site.register(Activity)
+admin.site.register(Decision)
+admin.site.register(Report)
